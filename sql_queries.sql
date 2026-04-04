2940bbf4c31325077e31c04d7fc2d1b627b414f5
kashishharora <150424377+kashishharora@users.noreply.github.com>
2026-04-04 19:04:38 +0530

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,11 @@
-
+### 🔹Total number of sales transactions
+SELECT COUNT(*) AS total_transactions
+FROM sales.transactions;
+### 🔹 Sales Records for Market Code 'Mark0001'
+SELECT *
+FROM sales.transactions
+WHERE market_code = 'Mark001';
+### 🔹 Total Transactions with USD Currency
+SELECT *
+FROM sales.transactions
+WHERE currency = 'USD';
